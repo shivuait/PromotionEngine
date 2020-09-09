6ba6e240f0c160f2ebf2997dd9e49ddf23fc7b9d
Shivakumar <shivuait@gmail.com>
2020-09-10 00:21:35 +0530

    Removed unnecessary usings.

--- Shopping/Repository/Abstraction/ICartRepository.cs
+++ Shopping/Repository/Abstraction/ICartRepository.cs
@@ -1,13 +1,6 @@
-﻿using DomainModels.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Repository.Abstraction
+﻿namespace Repository.Abstraction
 {
-  public interface ICartRepository
+    public interface ICartRepository
     {
         void AddCart(int productId, string name, int unitPrice, int quantity);
         void DeleteCart(int productId);
--- Shopping/UnitTestProjectShopping/CartUnitTest.cs
+++ Shopping/UnitTestProjectShopping/CartUnitTest.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using DomainModels.Entities;
 using Repository.Abstraction;
 using Repository.Implementation;
 
